e99ab1dbd83a5e07f2f985c108760c936c5fab4d
Vld-NN <vnosov1@gmail.com>
2024-09-08 00:06:05 +0300

    added class Actor plays\models

--- plays/models.py
+++ plays/models.py
@@ -6,7 +6,13 @@ class Play(models.Model):
     title = models.CharField(max_length=255)
     annotations = models.TextField()
 
+class Actor(models.Model):
+    name = models.CharField(max_length=255)
+    birthday = models.DateField(null=True)
 
-class Show(models.Model)
+
+class Show(models.Model):
     starts_at = models.DateTimeField()
     play = models.ForeignKey(Play, on_delete=models.CASCADE)
+    actor = models.ManyToManyField(Actor.)
+
