4c0c65b172a3dfac8b4c20deac12cc1bfcef16a0
Vld-NN <vnosov1@gmail.com>
2024-09-07 23:59:04 +0300

    modifief plays\models

--- plays/models.py
+++ plays/models.py
@@ -5,3 +5,8 @@ from django.db import models
 class Play(models.Model):
     title = models.CharField(max_length=255)
     annotations = models.TextField()
+
+
+class Show(models.Model)
+    starts_at = models.DateTimeField()
+    play = models.ForeignKey(Play, on_delete=models.CASCADE)
