97ccf89973d1208ce7bb398ef023423656c04eac
Vld-NN <vnosov1@gmail.com>
2024-09-18 23:46:38 +0300

    modified project

--- plays/urls.py
+++ plays/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from theatre.urls import urlpatterns
+#from theatre.urls import urlpatterns
 
 from plays.views import ShowDetailView
 
 urlpatterns = [
-    path('<int:pk>', ShowDetailView.as.view())
+    path('<int:pk>', ShowDetailView.as_view(), name='show_detail')
 ]
